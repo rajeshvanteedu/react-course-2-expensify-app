fe17630fb968b96a2c0b3f48b2d964f84cc05249
VANTEEDU Rajeshwar <rajeshwar.vanteedu@hexagon.com>
2018-09-11 15:53:04 +0530

    producion webpack build

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore'
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
 import {setTextFilter} from './actions/filters';
 import getVisisbleExpneses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 const store = configureStore();
 
